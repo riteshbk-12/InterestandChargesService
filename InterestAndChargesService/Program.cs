09979f0967ffeadd220f35a94b60e378046bdf94
Ritesh Kandalgaonkar <154956526+riteshbk-12@users.noreply.github.com>
2026-02-26 14:39:30 +0530

    Change scheduled time for penalty and interest jobs

--- InterestAndChargesService/Program.cs
+++ InterestAndChargesService/Program.cs
@@ -42,14 +42,14 @@ app.UseHangfireDashboard("/hangfire", new DashboardOptions
 RecurringJob.AddOrUpdate<PenaltyJobService>(
     "daily penalty",
      job => job.CalculatePenaltyAsync(),
-    "25 14 * * *",
+    "44 14 * * *",
     TimeZoneInfo.FindSystemTimeZoneById("India Standard Time")
     );
 
 RecurringJob.AddOrUpdate<InterestAccrualJobService>(
     "daily interest accrual",
     job => job.CalculateInterestAccrual(),
-    "25 14 * * *",
+    "44 14 * * *",
     TimeZoneInfo.FindSystemTimeZoneById("India Standard Time")
 
     );
