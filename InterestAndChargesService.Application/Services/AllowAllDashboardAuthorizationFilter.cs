ad91ed057448064cb15a6206a5533cb0d9c63d16
sahil3792 <sahilharshalv@gmail.com>
2026-02-26 14:13:54 +0530

    created authorization class

---
+++ InterestAndChargesService.Application/Services/AllowAllDashboardAuthorizationFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hangfire.Annotations;
+using Hangfire.Dashboard;
+
+namespace InterestAndChargesService.Application.Services
+{
+    public class AllowAllDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
